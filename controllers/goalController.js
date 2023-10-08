5ed1f9cc8710bafbbe0763a9bb23d55faa39545c
Abuswaleh <abuswaleh1995@gmail.com>
2023-10-08 10:41:21 +0530

    first

--- controllers/goalController.js
+++ controllers/goalController.js
@@ -1,141 +1,213 @@
-const Goal = require('../models/goalModel');
+const Goal = require("../models/goalModel");
 
 const createGoal = async (req, res) => {
-  try {
-    // TODO: Implement logic to create a new goal
-    // Retrieve data from req.body, create a new goal, and save it to the database
-    // Example response when goal is created successfully:
-    // res.status(201).json({ message: 'Goal created successfully', goal });
-  } catch (error) {
-    res
-      .status(500)
-      .json({ message: 'Internal server error', error: error.message });
-  }
+	try {
+		// TODO: Implement logic to create a new goal
+		// Retrieve data from req.body, create a new goal, and save it to the database
+		// Example response when goal is created successfully:
+		// res.status(201).json({ message: 'Goal created successfully', goal });
+		const { name, description, target, deadline, type } = req.body;
+		const goal = await Goal.create({
+			name,
+			description,
+			target,
+			deadline,
+			type,
+		});
+		res.status(201).json({ message: "Goal created successfully", goal });
+	} catch (error) {
+		res.status(500).json({
+			message: "Internal server error",
+			error: error.message,
+		});
+	}
 };
 
 const getGoals = async (req, res) => {
-  try {
-    // TODO: Implement logic to retrieve all goals
-    // Retrieve all goals from the database
-    // Example response when goals are found:
-    // res.status(200).json(goals);
-    // Example response when no goals are found:
-    // res.status(404).json({ message: 'No goals found' });
-  } catch (error) {
-    res
-      .status(500)
-      .json({ message: 'Internal server error', error: error.message });
-  }
+	try {
+		// TODO: Implement logic to retrieve all goals
+		// Retrieve all goals from the database
+		// Example response when goals are found:
+		// res.status(200).json(goals);
+		// Example response when no goals are found:
+		// res.status(404).json({ message: 'No goals found' });
+		const goals = await Goal.find();
+		if (goals.length > 0) {
+			res.status(200).json(goals);
+		} else {
+			res.status(404).json({ message: "No goals found" });
+		}
+	} catch (error) {
+		res.status(500).json({
+			message: "Internal server error",
+			error: error.message,
+		});
+	}
 };
 
 const getGoalById = async (req, res) => {
-  const goalId = req.params.id;
+	const goalId = req.params.id;
 
-  try {
-    // TODO: Implement logic to retrieve a goal by ID
-    // Use Goal.findById(goalId) to retrieve a goal
-    // Example response when goal is found:
-    // res.status(200).json(goal);
-    // Example response when goal is not found:
-    // res.status(404).json({ message: 'Goal not found' });
-  } catch (error) {
-    res
-      .status(500)
-      .json({ message: 'Internal server error', error: error.message });
-  }
+	try {
+		// TODO: Implement logic to retrieve a goal by ID
+		// Use Goal.findById(goalId) to retrieve a goal
+		// Example response when goal is found:
+		// res.status(200).json(goal);
+		// Example response when goal is not found:
+		// res.status(404).json({ message: 'Goal not found' });
+
+		const goal = await Goal.findById(goalId);
+		if (goal) {
+			res.status(200).json(goal);
+		} else {
+			res.status(404).json({ message: "Goal not found" });
+		}
+	} catch (error) {
+		res.status(500).json({
+			message: "Internal server error",
+			error: error.message,
+		});
+	}
 };
 
 const updateGoal = async (req, res) => {
-  const goalId = req.params.id;
-  const updateInfo = req.body;
+	const goalId = req.params.id;
+	const updateInfo = req.body;
 
-  try {
-    // TODO: Implement logic to update a goal
-    // Use Goal.findByIdAndUpdate(goalId, updateInfo, { new: true }) to update the goal
-    // Example response when goal is updated successfully:
-    // res.status(200).json({ message: 'Goal updated successfully', goal: updatedGoal });
-    // Example response when goal is not found:
-    // res.status(404).json({ message: 'Goal not found' });
-  } catch (error) {
-    res
-      .status(500)
-      .json({ message: 'Internal server error', error: error.message });
-  }
+	try {
+		// TODO: Implement logic to update a goal
+		// Use Goal.findByIdAndUpdate(goalId, updateInfo, { new: true }) to update the goal
+		// Example response when goal is updated successfully:
+		// res.status(200).json({ message: 'Goal updated successfully', goal: updatedGoal });
+		// Example response when goal is not found:
+		// res.status(404).json({ message: 'Goal not found' });
+		const updatedGoal = await Goal.findByIdAndUpdate(goalId, updateInfo, {
+			new: true,
+		});
+		if (updatedGoal) {
+			res.status(200).json({
+				message: "Goal updated successfully",
+				goal: updatedGoal,
+			});
+		} else {
+			res.status(404).json({ message: "Goal not found" });
+		}
+	} catch (error) {
+		res.status(500).json({
+			message: "Internal server error",
+			error: error.message,
+		});
+	}
 };
 
 const deleteGoal = async (req, res) => {
-  const goalId = req.params.id;
+	const goalId = req.params.id;
 
-  try {
-    // TODO: Implement logic to delete a goal
-    // Use Goal.findByIdAndDelete(goalId) to delete the goal
-    // Example response when goal is deleted successfully:
-    // res.status(200).json({ message: 'Goal deleted successfully', goal: deletedGoal });
-    // Example response when goal is not found:
-    // res.status(404).json({ message: 'Goal not found' });
-  } catch (error) {
-    res
-      .status(500)
-      .json({ message: 'Internal server error', error: error.message });
-  }
+	try {
+		// TODO: Implement logic to delete a goal
+		// Use Goal.findByIdAndDelete(goalId) to delete the goal
+		// Example response when goal is deleted successfully:
+		// res.status(200).json({ message: 'Goal deleted successfully', goal: deletedGoal });
+		// Example response when goal is not found:
+		// res.status(404).json({ message: 'Goal not found' });
+		const deletedGoal = await Goal.findByIdAndDelete(goalId);
+		if (deleteGoal) {
+			res.status(200).json({
+				message: "Goal deleted successfully",
+				goal: deletedGoal,
+			});
+		} else {
+			res.status(404).json({ message: "Goal not found" });
+		}
+	} catch (error) {
+		res.status(500).json({
+			message: "Internal server error",
+			error: error.message,
+		});
+	}
 };
 
 const sortGoals = async (req, res) => {
-  const order = req.params.order;
+	const order = req.params.order;
 
-  try {
-    // TODO: Implement logic to sort goals by target
-    // Retrieve and sort goals from the database based on the 'order' parameter
-    // Example response when goals are sorted:
-    // res.status(200).json(sortedGoals);
-  } catch (error) {
-    res
-      .status(500)
-      .json({ message: 'Internal server error', error: error.message });
-  }
+	try {
+		// TODO: Implement logic to sort goals by target
+		// Retrieve and sort goals from the database based on the 'order' parameter
+		// Example response when goals are sorted:
+		// res.status(200).json(sortedGoals);
+		const goals = await Goal.find();
+		if (goals.length > 0) {
+			res.status(200).json(goals);
+		} else {
+			res.status(404).json({ message: "No goals found" });
+		}
+	} catch (error) {
+		res.status(500).json({
+			message: "Internal server error",
+			error: error.message,
+		});
+	}
 };
 
 const getGoalsByType = async (req, res) => {
-  const goalType = req.params.type;
+	const goalType = req.params.type;
 
-  try {
-    // TODO: Implement logic to retrieve goals by type
-    // Retrieve goals from the database based on the 'goalType' parameter
-    // Example response when goals are found:
-    // res.status(200).json(goals);
-    // Example response when no goals are found:
-    // res.status(404).json({ message: 'No goals found for the given type' });
-  } catch (error) {
-    res
-      .status(500)
-      .json({ message: 'Internal server error', error: error.message });
-  }
+	try {
+		// TODO: Implement logic to retrieve goals by type
+		// Retrieve goals from the database based on the 'goalType' parameter
+		// Example response when goals are found:
+		// res.status(200).json(goals);
+		// Example response when no goals are found:
+		// res.status(404).json({ message: 'No goals found for the given type' });
+		const goals = await Goal.find({ type: goalType });
+		if (goals.length > 0) {
+			res.status(200).json(goals);
+		} else {
+			res.status(404).json({
+				message: "No goals found for the given type",
+			});
+		}
+	} catch (error) {
+		res.status(500).json({
+			message: "Internal server error",
+			error: error.message,
+		});
+	}
 };
 
 const getGoalsByDeadline = async (req, res) => {
-  const deadline = req.params.deadline;
+	const deadline = req.params.deadline;
 
-  try {
-    // TODO: Implement logic to retrieve goals by deadline
-    // Retrieve goals from the database based on the 'deadline' parameter
-    // Example response when goals are found:
-    // res.status(200).json(goals);
-    // Example response when no goals are found:
-    // res.status(404).json({ message: 'No goals found before the given deadline' });
-  } catch (error) {
-    res
-      .status(500)
-      .json({ message: 'Internal server error', error: error.message });
-  }
+	try {
+		// TODO: Implement logic to retrieve goals by deadline
+		// Retrieve goals from the database based on the 'deadline' parameter
+		// Example response when goals are found:
+		// res.status(200).json(goals);
+		// Example response when no goals are found:
+		// res.status(404).json({ message: 'No goals found before the given deadline' });
+		const goals = await Goal.find({ deadline });
+		if (goals.length > 0) {
+			res.status(200).json(goals);
+		} else {
+			res.status(404).json({
+				message: "No goals found before the given deadline",
+			});
+		}
+	} catch (error) {
+		res.status(500).json({
+			message: "Internal server error",
+			error: error.message,
+		});
+	}
 };
 
 module.exports = {
-  createGoal,
-  getGoals,
-  getGoalById,
-  updateGoal,
-  deleteGoal,
-  sortGoals,
-  getGoalsByType,
-  getGoalsByDeadline,
+	createGoal,
+	getGoals,
+	getGoalById,
+	updateGoal,
+	deleteGoal,
+	sortGoals,
+	getGoalsByType,
+	getGoalsByDeadline,
 };
--- models/goalModel.js
+++ models/goalModel.js
@@ -1,7 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const goalSchema = new mongoose.Schema({
-  //
+	name: { type: String, required: true },
+	description: { type: String },
+	target: { type: Number, required: true },
+	deadline: { type: Date, required: true },
+	type: { type: String, required: true },
 });
-
+const Goal = mongoose.model(goalSchema);
 module.exports = Goal;
